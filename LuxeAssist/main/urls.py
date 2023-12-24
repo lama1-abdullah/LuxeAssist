8bb861f93d516a3f3b2e1083403689f066c1b58e
lama1-abdullah <lama.alothemen21@gmail.com>
2023-12-24 11:18:55 +0300

    edit

--- LuxeAssist/main/urls.py
+++ LuxeAssist/main/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
    path("", views.home_view, name="home_view"),
    path("about/", views.about_view, name="about_view"),
    path("contact/", views.contact_view, name="contact_view"),
-   path("payment/", views.payment_view, name="payment_view"),
-
+   path("payment/", views.payment_view, name="payment_view")
 ]
