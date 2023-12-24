010c72761892d11ae334f7e35de2e910f3d70388
lama1-abdullah <lama.alothemen21@gmail.com>
2023-12-24 10:11:59 +0300

    pag+ navbar

--- LuxeAssist/main/views.py
+++ LuxeAssist/main/views.py
@@ -4,6 +4,14 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 def home_view(request: HttpRequest):
 
-
-
     return render(request, "main/home.html")
+
+
+def about_view(request: HttpRequest):
+
+    return render(request, "main/about.html")
+
+
+def contact_view(request: HttpRequest):
+
+    return render(request, "main/contact.html")
