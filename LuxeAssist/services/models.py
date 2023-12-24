772d2cbf55014fa89c438ff74e0d4d9f79b99926
Reef <reef.zayed2@gmail.com>
2023-12-24 10:55:16 +0300

    add services app

---
+++ LuxeAssist/services/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here
+
+
+class TypeService(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to="image/", default="image/default_TypeService.jpg")
+    title=models.CharField(max_length=256,default="")
+    description=models.TextField(default="")
+
+
+
+
+class Service(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    type_service=models.ForeignKey(TypeService,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to="image/", default="image/defaultService.jpg")
+    title=models.CharField(max_length=256,default="")
+    description=models.TextField(default="")
+    initial_price=models.PositiveIntegerField(default=0)
