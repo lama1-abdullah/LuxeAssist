b7581f3310d93889b6674fc35f808bb990f2387f
Reef <reef.zayed2@gmail.com>
2023-12-24 14:13:42 +0300

    add functions in app services

--- LuxeAssist/services/urls.py
+++ LuxeAssist/services/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "services"
 
 urlpatterns = [
-    
+    path('add/', views.add_typeService_view , name="add_typeService_view"),
+    path('home/', views.home_services_view, name="home_services_view")
 ]
--- LuxeAssist/services/views.py
+++ LuxeAssist/services/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from .models import TypeService
 # Create your views here.
+
+
+
+def add_typeService_view(request:HttpRequest):
+
+    if request.method=="POST":
+        new_service=TypeService(title=request.POST["title"],description=request.POST["description"])
+        if "image" in request.FILES:
+            new_service.image=request.FILES["image"]
+        new_service.save()
+        return redirect("services:home_services_view")
+        
+    return render(request,"services/add_typeService.html")
+
+
+def home_services_view(request:HttpRequest):
+
+    typeServices=TypeService.objects.all()
+
+    return render(request, 'services/home_service.html', {"typeService":typeServices})
+
+
+
+def details_typeService_view(request:HttpRequest, typeservice_id):
+
+    typeServices=TypeService.objects.get(id=typeservice_id)
+
+    return render(request , "services/details_typeService.html",{"typeService":typeServices})
+
+def add_service_view(request:HttpRequest):
+    pass
