a85fe8e749c250996aa23c694ef93e9d15b49fcf
ebtesam <ebtesam167@hotmail.com>
2023-12-24 14:09:48 +0300

    methods for request

--- LuxeAssist/request/views.py
+++ LuxeAssist/request/views.py
@@ -1,2 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+from .models import Request
+from services.models import Service
+
+
+def add_Request_view(request:HttpRequest, service_id):
+        #if not request.user.is_authenticated:
+              # return render(request, "", status=401)
+        
+    service = Service.objects.get(id = service_id)  
+    if request.method == "POST":
+        newRequest = Request(user = request.user, service = service, note = request.POST["note"], date = request.POST["date"], request_price = request.POST["request_price"])
+        newRequest.save()
+        return redirect("request:request_details_view", newRequest.id)
+        
+
+def user_requests_view(request: HttpRequest):
+   # try:
+        
+    requests = Request.objects.get(user= request.user)
+
+    return render(request, 'request/user_requests_view.html', {"requests" : requests})
+    #except Exception as e:
+
+      #  return render(request, "")
+    
+
+def concierge_requests_view(request: HttpRequest):
+   # try:
+        
+       request = Request.objects.filter(user = request.user)
+
+       return render(request, 'request/concierge_requests_view.html', {"requests" : request})
+    #except Exception as e:
+
+       # return render(request, "")
+    
+
+def admin_requests_view(request: HttpRequest):
+  # try: 
+      request = Request.objects.order_by('-createdAt')
+     
+     
+      return render(request ,"request/admin_requests_view.html" , {"requests" : request})
+   #except Exception as e:
+
+       # return render(request, "")
+   
+
+def request_details_view(request: HttpRequest):
+    pass 
+
+def cancel_request_view(request: HttpRequest, requset_id):
+    ## try:
+            request = Request.objects.get(id = requset_id)
+            request.delete()
+            return redirect("service:")
+        
+     ##except  Exception as e:
+
+        ## return render(request, "")
 
