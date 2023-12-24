30181324ddf382107d467cd5bd4f56552256ed70
lama1-abdullah <lama.alothemen21@gmail.com>
2023-12-24 11:13:26 +0300

    edite

--- LuxeAssist/main/urls.py
+++ LuxeAssist/main/urls.py
@@ -6,7 +6,6 @@ app_name = "main"
 urlpatterns = [
    path("", views.home_view, name="home_view"),
    path("about/", views.about_view, name="about_view"),
-   path("contact/", views.contact_view, name="contact_view"),
-   path("payment/", views.payment_view, name="payment_view"),
-
+   path("contact/", views.contact_view, name="contact_view")
+   
 ]
