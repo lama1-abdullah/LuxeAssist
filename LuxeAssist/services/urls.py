f9e492c8bbed730d2edc11ee3f0f6236d63e3d6e
Reef <reef.zayed2@gmail.com>
2023-12-25 03:48:15 +0300

    add functions in app services

--- LuxeAssist/services/urls.py
+++ LuxeAssist/services/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = "services"
 
 urlpatterns = [
-    path('add/', views.add_typeService_view , name="add_typeService_view"),
-    path('home/', views.home_services_view, name="home_services_view")
+    path('add/type', views.add_typeService_view , name="add_typeService_view"),
+    path('home/', views.home_services_view, name="home_services_view"),
+    path('type/details/<typeservice_id>',views.details_typeService_view, name="details_typeService_view"),
+    path('update/type/<typeservice_id>',views.update_typeService_view, name="update_typeService_view"),
+    path('delete/<typeservice_id>', views.delete_stable_views,name="delete_stable_views"),
+    path('add/<typeservice_id>',views.add_service_view , name="add_service_view")
 ]
--- LuxeAssist/services/views.py
+++ LuxeAssist/services/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.models import User
-from .models import TypeService
+from .models import TypeService , Service
 # Create your views here.
 
 
@@ -16,6 +16,8 @@ def add_typeService_view(request:HttpRequest):
     return render(request,"services/add_typeService.html")
 
 
+
+
 def home_services_view(request:HttpRequest):
 
     typeServices=TypeService.objects.all()
@@ -24,11 +26,59 @@ def home_services_view(request:HttpRequest):
 
 
 
+
+
 def details_typeService_view(request:HttpRequest, typeservice_id):
 
     typeServices=TypeService.objects.get(id=typeservice_id)
+    services=Service.objects.filter(type_service=typeServices)
+    return render(request , "services/details_typeService.html",{"typeService":typeServices , "services":services})
 
-    return render(request , "services/details_typeService.html",{"typeService":typeServices})
 
-def add_service_view(request:HttpRequest):
-    pass
+
+
+
+def update_typeService_view(request:HttpRequest,typeservice_id):
+   
+    typeService= TypeService.objects.get(id=typeservice_id)
+
+    if request.method=="POST":
+        
+        typeService.title=request.POST["title"]
+        typeService.description=request.POST["description"]
+        typeService.image=request.FILES["image"]
+        typeService.save()
+
+        return redirect("services:details_typeService_view",typeservice_id=typeService.id)
+        
+
+        
+    return render(request,"services/update_typeServices.html",{"typeService":typeService })
+
+
+
+# ****************************************error in delete_stable_views and add_service_view***************************************************
+def delete_stable_views(request:HttpRequest, typeService_id):
+    typeService= TypeService.objects.get(id=typeService_id)
+    typeService.delete()
+
+    return redirect( "services:home_services_view")
+
+def add_service_view(request:HttpRequest,typeservice_id):
+
+    services=Service.objects.get(id=typeservice_id)
+
+    if request.method== "POST":
+        new_service=Service(user=request.user, type_service=services , title=request.POST["title"],description=request.POST["description"], image=request.FILES["image"] , initial_price=request.POST["initial_price"])
+        new_service.save()
+
+        return redirect("service:details_typeService_view", typeservice_id=services.id)
+
+    
+    return render(request , "services/add_service.html", {"service":services})
+
+
+
+
+
+        
